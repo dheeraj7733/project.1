f3caff7b5f280702c4a922e1a9878c7207e63129
Dheeraj Sharma <djsharma2189t@gmail.com>
2025-12-12 15:06:14 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,24 @@
 
+fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById("header").innerHTML = data;
+          
+      let currentpage = window.location.pathname.split("/").pop();
+          if (!currentpage || currentpage == "#"){
+            currentpage = "index.html";
+          }
+
+      let links = document.querySelectorAll(".menu .menubar ul li a");
+
+      links. forEach(link => {
+        console.log("Current page:" , currentpage);
+
+        let linkpage = link.getAttribute("href");
+        console.log("attribute value:" ,linkpage);
+        if(!linkpage)return;
+        if (linkpage == currentpage){
+            link.closest("li").classList.add("underline_current")
+        }
+      });
+   });
