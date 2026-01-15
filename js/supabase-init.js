044339565ca5ecfe2c9bf8533e516242828fa251
Dheeraj Sharma <djsharma2189t@gmail.com>
2026-01-15 10:14:21 +0530

    Update supabase-init.js

--- js/supabase-init.js
+++ js/supabase-init.js
@@ -1,1 +1,11 @@
+const supabaseClient = window.supabase.createClient(
+  "sb_publishable_wmOXVyoaOKdG4DLoCQ7dHg_lkmMcg8c",
+  "https://sdzbbellnefpqwxqjzqz.supabase.co"
+);
 
+if (supabaseClient){
+  consoe.log("database connected");
+}
+else{
+   consoe.log("database not connected");
+}
