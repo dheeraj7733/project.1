01875480845dc6fc584b597530afa8600af3ec00
Dheeraj Sharma <djsharma2189t@gmail.com>
2026-01-15 11:25:00 +0530

    Update users.js

--- js/users.js
+++ js/users.js
@@ -1,1 +1,13 @@
-document.getElementById("")
+document.getElementById("user form").addEventListner("submit",async(e){
+  e.preventDefult();
+
+  const name = document.getElementById("name").value.trin();
+  const email = document.getElementById("emali").value.trin().tolowerCase();
+  const password = document.getElementById("password").value.trin();
+
+  if(!name || !email || !password){
+      alert("All fields are required");
+    
+    
+}
+/*Check if email already exit  
